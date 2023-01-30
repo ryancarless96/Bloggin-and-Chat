1f52d545fc4406ecba92fde80b6b9a90551755d6
Ryan Carless <ryancarless96@gmail.com>
2023-01-30 16:24:03 -0500

    commit changes

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const blogRoutes = require('./blog-routes');
 
-router.use('/users', userRoutes);
-router.use('/blogs', blogRoutes);
+
+const userRoutes = require('./userRoutes.js');
+const blogRoutes = require('./blogRoutes.js');
+
+router.use('/users', userRoutes.js);
+router.use('/blogs', blogRoutes.js);
 
 
 module.exports = router;
---
+++ Develop/controllers/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes.js');
+
+router.use('/', homeRoutes.js);
+router.use('/api',apiRoutes)
